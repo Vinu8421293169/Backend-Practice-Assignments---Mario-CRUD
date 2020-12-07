08037f729f460ba5743d1542cc970d48a1a50811
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-12-08 00:56:11 +0530

    Update marioChar.js

--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 //  Your code goes here
+const MarioModel=new mongoose.model('mariochar',new mongoose.Schema({name:{type:String,default:"Luigi"}, weight:{type:Number,default:60} }));
 
-
-module.exports = marioModel;
+module.exports = MarioModel;
